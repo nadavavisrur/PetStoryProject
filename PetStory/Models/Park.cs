cf57bcdc0a9327b2f768bb841ead60408efeeea7
Yasmin <simmons.yasmin@gmail.com>
2018-08-26 20:26:35 +0300

    models

---
+++ PetStory/Models/Park.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetStory.Models
+{
+    public class Park
+    {
+        public int ID { set; get; }
+        public string name { set; get; }
+        public int zipCode { set; get; }
+        public string address { set; get; }
+        //public List<String> facilities { set; get; }
+        public int likes { set; get; }
+
+    }
+}
